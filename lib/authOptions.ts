3b91ee27356d5e897adcf70fc05be491234f9723
Ishan Saini <ishansaini0105@gmail.com>
2025-08-08 17:59:45 +0530

    The auth is done

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,6 +1,8 @@
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, User } from "next-auth";
+import { prisma } from "./DB";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -23,7 +25,82 @@ export const authOptions: NextAuthOptions = {
         if (!credentials?.email || !credentials.password) {
           throw new Error("Email or passwordf is wrong");
         }
+
+        try {
+          const existUser = await prisma.user.findUnique({
+            where: {
+              email: credentials.email,
+            },
+          });
+          if (!existUser) {
+            throw new Error("User not exist");
+          }
+
+          const samePassword = await bcrypt.compare(
+            existUser.password,
+            credentials.password
+          );
+
+          if (!samePassword) {
+            throw new Error("Email or passwordf is wrong");
+          }
+
+          return {
+            id: existUser.id.toString(),
+            email: existUser.email,
+            username: existUser.username,
+            success: true,
+          } as User;
+        } catch (error) {
+          if (error instanceof Error) {
+            console.log("Auth Error:", error.message);
+            return null;
+          }
+        }
+        return null;
       },
     }),
   ],
+
+  pages: {
+    signIn: "/signin",
+    signOut: "/signout",
+    error: "/error",
+  },
+  session: {
+    strategy: "jwt",
+
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user && user.email) {
+        const dbUser = await prisma.user.findUnique({
+          where: {
+            email: user.email,
+          },
+        });
+        if (!dbUser) {
+          // Add a custom flag to token
+          token.registerRedirect = true;
+          return token;
+        }
+
+        token.id = dbUser.id.toString();
+        token.username = dbUser.username;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user.id = token.id), (session.user.username = token.username);
+      }
+      return session;
+    },
+  },
+  secret: process.env.JWT_SECRET,
 };
